8ad42635541ec35ecbe02ab60f4223639748645f
borgsid <s-i-l@hotmail.it>
2023-05-07 17:54:41 +0200

    links

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Voleeyo: linkein for volunteers" key="title"/>
+        <meta property="og:title" content="Voleeyo: the LinkedIn for volunteers" key="title"/>
         <meta property="og:description" content="built at buildspace during nights and weekeds" key="description"/>
         <meta
           property="og:image"
-          content="https://drive.google.com/file/d/1VQbZAVF_o5WGDYfbvHclrRbj2s8Hk3BD/view?usp=sharing"
+          content="https://voleeyo-1.azurewebsites.net//_next/static/preview.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
