2b4d3d19525127cf1178bb1596f9eeeb71ed04f4
borgsid <s-i-l@hotmail.it>
2023-08-29 00:52:34 +0200

    deleting user from auth 0, disabling account deskr

--- pages/api/user/userProfileSettings/privacy/[userId].js
+++ pages/api/user/userProfileSettings/privacy/[userId].js
@@ -2,31 +2,74 @@ import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async (req, res) => {
   const session = await getSession(req, res);
-  const UtcDate = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}T${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getUTCSeconds()}Z`;
+  const UtcDate = `${new Date().toISOString()}`; // Use ISO format for UTC date
   const { user } = session;
-  if (user) {
-    var queryString = `?where=[{"attribute":"userId","operator":"=","value":"${req.query?.userId}"}]`;
-    var currentUserRaw = await fetch(`${process.env.DESKREE_BASE_URL}/volunteers${queryString}`, {
-      method: "get",
-    });
-    if (currentUserRaw.status == 200) {
-      const currentUser = (await currentUserRaw.json()).data[0];
-      var logicDeleteUserRaw = await fetch(`${process.env.DESKREE_BASE_URL}/volunteers/${currentUser.uid}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          deletionUTCdate: UtcDate,
-          isActive: false
-        }),
-        headers: {
-          "content-type": "application/json; charset=utf-8"
-        }
-      });
-      res.status(logicDeleteUserRaw.status).json(`User is ${logicDeleteUserRaw.status!=200?"not ":""}deleted`);
 
+  if (user) {
+    try {
+      const queryString = `?where=[{"attribute":"userId","operator":"=","value":"${req.query?.userId}"}]`;
+      const currentUserRaw = await fetch(`${process.env.DESKREE_BASE_URL}/volunteers${queryString}`, {
+        method: "GET",
+      });
+
+      if (currentUserRaw.status === 200) {
+        const currentUser = (await currentUserRaw.json()).data[0];
+
+        // Logic to mark the user as inactive in your application's database
+        const logicDeleteUserRaw = await fetch(`${process.env.DESKREE_BASE_URL}/volunteers/${currentUser.uid}`, {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json; charset=utf-8"
+          },
+          body: JSON.stringify({
+            deletionUTCdate: UtcDate,
+            isActive: false
+          })
+        });
+        if (logicDeleteUserRaw.status === 200) {
+          // Fetch an access token for Auth0 Management API
+          const tokenRaw = await fetch(`${process.env.AUTH0_ISSUER_BASE_URL}/oauth/token`, {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json' // Correct content-type header
+            },
+            body: JSON.stringify({
+              grant_type: 'client_credentials',
+              client_id: process.env.AUTH0_M_M_CLIENT_ID,
+              client_secret: process.env.AUTH0_M_M_CLIENT_SECRET,
+              audience: `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/`
+            })
+          });
+          if (tokenRaw.status == 200) {
+
+            const token = await tokenRaw.json();
+
+            var accesstoken = token.access_token;
+            var bearer = token.token_type
+            const resppRaw = await fetch(`${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/users/${user.sub}`, {
+              method: "DELETE",
+              // method: "PATCH",
+              headers: {
+                "content-type": "application/json",
+                "Authorization": `${bearer} ${accesstoken}`,
+              },
+              // body: JSON.stringify({ blocked: true })
+            });
+            res.status(logicDeleteUserRaw.status).json(`User is ${resppRaw.status > 204 ? "not " : ""}deleted`);
+          }
+          else
+            res.status(400).json({});
+        }
+        else
+        res.status(400).json({});
+
+      } else {
+        res.status(400).json({});
+      }
+    } catch (error) {
+      res.status(500).json({ error: "An error occurred" });
     }
-    else
-      res.status(400).json({});
-  }
-  else
+  } else {
     res.status(400).json({});
-}); 
+  }
+});
